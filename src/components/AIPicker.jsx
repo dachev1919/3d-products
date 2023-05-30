183d2ceb68a7fe2f0202aee6256780820386f5ce
Dachev <dachev1919@gmail.com>
2023-05-30 18:20:03 +0300

    Created components for customizer page

---
+++ src/components/AIPicker.jsx
@@ -0,0 +1,9 @@
+const AIPicker = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default AIPicker;
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,8 +1,41 @@
-const Customizer = () => {
-  return (
-    <div>
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store/index.js";
+import { fadeAnimation, slideAnimation } from "../config/motion.js";
+import { CustomButton, Tab } from "../components/index.js";
+import { EditorTabs } from "../config/constants.js";
 
-    </div>
+const Customizer = () => {
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div key='custom' className='absolute top-0 left-0 z-10' {...slideAnimation('left')}>
+            <div className='flex items-center min-h-screen'>
+              <div className='editortabs-container tabs'>
+                {EditorTabs.map(tab => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+            <CustomButton
+              type='filled'
+              title='Go Back'
+              handleClick={() => state.intro = true}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+            />
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
